8c20cb724d7e84f3d9ddf28665ded190feecaf88
Ahmed Alshakh <ahmed.s.alshakh@gmail.com>
2019-03-17 20:04:08 +0300

    simple corrospondance through websocket

--- client/main.js
+++ client/main.js
@@ -1,20 +1,28 @@
 
 
 
+
+
+
+
+
 document.querySelectorAll('[data-malafat]').forEach((el) => {
     let dir = el.dataset['dir']
 
 
-    let ws = new WebSocket('ws://localhost:3000/malafat/test');
-    ws.onopen = function () {
+
+    const socketUrl = `${(location.protocol === 'https:') ? 'wss://' : 'ws://'}${location.hostname}${location.port ? `:${location.port}` : ''}/malafat/111`;
+    const socket = new WebSocket(socketUrl);
+
+    socket.onopen = function () {
         console.log('websocket is connected ...')
-        ws.send(JSON.stringify({
+        socket.send(JSON.stringify({
             "type" : "get-dir",
             "dir" : dir
         }))
     }
 
-    ws.onmessage = function (ev) {
+    socket.onmessage = function (ev) {
         el.innerHTML = ev['data']
         console.log(ev)
     }
--- index.js
+++ index.js
@@ -4,15 +4,30 @@ var expressws = require('express-ws')
 var app = express()
 var expressWs = expressws(app);
 
+const PathTree = require('./js/pathtree.js')
+
+
+
+
 app.use(express.static(__dirname + "/client"));
 
 expressWs.app.ws('/malafat/:id', function (ws, req) {
   ws.on('message', (message) => {
-      let command = JSON.parse(message)
+      let command = {}
+      try {
+          command = JSON.parse(message)
+      } catch (e) {
+          console.error("error happened",e)
+          return
+      }
+
       console.log("Received message =>", command)
 
       if (command['type'] === "get-dir") {
-          ws.send(command['dir'])
+          PathTree.pathTree(command['dir'],command['dir'])
+            .then( ( p ) => {
+                ws.send(JSON.stringify(p))
+            })
       }
   })
 
