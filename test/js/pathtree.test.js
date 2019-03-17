a9fbff9160a9e886ae957eb54cbf44ac37ef2712
Ahmed Alshakh <ahmed.s.alshakh@gmail.com>
2019-03-17 19:44:18 +0300

    prepare test dir

--- test/js/pathtree.test.js
+++ test/js/pathtree.test.js
@@ -2,8 +2,6 @@ let PrjRoot =`${require('path').dirname(require.main.filename)}/../..`;
 var PathTree = require(`${PrjRoot}/js/pathtree.js`)
 
 
-
-
 PathTree.pathTree(PrjRoot,PrjRoot)
     .then( r => {
         console.log(JSON.stringify(r,null,4))
