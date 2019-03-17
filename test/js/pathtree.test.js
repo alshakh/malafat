47330bc8ec2b6e7043880dd87559512a716f9507
Ahmed Alshakh <ahmed.s.alshakh@gmail.com>
2019-03-17 19:55:54 +0300

    fix test dir

--- test/js/pathtree.test.js
+++ test/js/pathtree.test.js
@@ -2,7 +2,9 @@ let PrjRoot =`${require('path').dirname(require.main.filename)}/../..`;
 var PathTree = require(`${PrjRoot}/js/pathtree.js`)
 
 
-PathTree.pathTree(PrjRoot,PrjRoot)
+console.log(PrjRoot)
+console.log(`${PrjRoot}/test/__sample-dir1`,`${PrjRoot}/test/__sample-dir1`)
+PathTree.pathTree(`${PrjRoot}/test/__sample-dir1`,`${PrjRoot}/test/__sample-dir1`)
     .then( r => {
         console.log(JSON.stringify(r,null,4))
     })
