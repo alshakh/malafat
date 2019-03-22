6d537ae06429faf66f88601a1d6140c7b3539681
Ahmed Alshakh <ahmed.s.alshakh@gmail.com>
2019-03-23 02:41:31 +0300

    separate client & server

--- example-client/main.js
+++
@@ -1,6 +0,0 @@
-const Malafat = require('./fileviewer')
-//
-const socketUrl = `${(location.protocol === 'https:') ? 'wss://' : 'ws://'}${location.hostname}${location.port ? `:${location.port}` : ''}/malafat`;
-document.querySelectorAll('[data-malafat]').forEach((el) => {
-    Malafat.fromElement(el, socketUrl)
-})
