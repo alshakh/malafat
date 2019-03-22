e53665258b0d10e599b247e5fd9b4c87e9f24605
Ahmed Alshakh <ahmed.s.alshakh@gmail.com>
2019-03-22 21:09:54 +0300

    move-all code to fileviewer

--- client/fileviewer.js
+++ client/fileviewer.js
@@ -87,6 +87,58 @@ function getIconColor(extension) {
 
 
 class FileViewer {
+
+    static fromElement ( element, socketUrl ) {
+        let dirpath = element.dataset.dir
+        let isWatch =  ((typeof element.dataset['watch']) !== 'undefined' ? true : false)
+        let isCreate =  ((typeof element.dataset['create']) !== 'undefined' ? true : false)
+        //
+        const socket = new WebSocket(socketUrl);
+        //
+        let fv = new FileViewer ({
+            element : element,
+            dir : dirpath,
+            requestFileContentFn : (f) => {
+                socket.send(JSON.stringify({
+                    "type" : "get-file-content",
+                    "path" : f,
+                }))
+            },
+            requestFileTreeFn : () => {
+                socket.send(JSON.stringify({
+                    "type" : "get-file-tree"
+                }))
+            },
+            fontsize : "40px"
+        })
+        //
+        socket.onmessage = function (ev) {
+            let response = JSON.parse(ev['data'])
+            switch ( response.type ) {
+                case "file-tree" :
+                    fv.recieveFileTree(response['file-tree'])
+                    break;
+                case "file-content" :
+                    fv.recieveFileContent(response['path'], response['content'])
+                    break;
+                case "file-changed" :
+                    fv.notifyFileChanged(response['path'])
+                    break
+            }
+        }
+        //
+        socket.onopen = () => {
+            console.log('websocket is connected ...')
+            socket.send(JSON.stringify({
+                "type" : "init",
+                "path" : dirpath,
+                "watch" : isWatch,
+                "create" : isCreate
+            }))
+            fv.newTree()
+        }
+    }
+
     constructor(options){
         this.element = options.element
         this.dir = options.dir
--- client/main.js
+++ client/main.js
@@ -1,57 +1,6 @@
 const FileViewer = require('./fileviewer')
-
-function processFileViewElement ( element ) {
-    let dirpath = element.dataset.dir
-    let isWatch =  ((typeof element.dataset['watch']) !== 'undefined' ? true : false)
-    let isCreate =  ((typeof element.dataset['create']) !== 'undefined' ? true : false)
-    //
-    const socketUrl = `${(location.protocol === 'https:') ? 'wss://' : 'ws://'}${location.hostname}${location.port ? `:${location.port}` : ''}/malafat`;
-    const socket = new WebSocket(socketUrl);
-    //
-    let fv = new FileViewer ({
-        element : element,
-        dir : dirpath,
-        requestFileContentFn : (f) => {
-            socket.send(JSON.stringify({
-                "type" : "get-file-content",
-                "path" : f,
-            }))
-        },
-        requestFileTreeFn : () => {
-            socket.send(JSON.stringify({
-                "type" : "get-file-tree"
-            }))
-        },
-        fontsize : "40px"
-    })
-    //
-    socket.onmessage = function (ev) {
-        let response = JSON.parse(ev['data'])
-        switch ( response.type ) {
-            case "file-tree" :
-                fv.recieveFileTree(response['file-tree'])
-                break;
-            case "file-content" :
-                fv.recieveFileContent(response['path'], response['content'])
-                break;
-            case "file-changed" :
-                fv.notifyFileChanged(response['path'])
-                break
-        }
-    }
-    //
-    socket.onopen = () => {
-        console.log('websocket is connected ...')
-        socket.send(JSON.stringify({
-            "type" : "init",
-            "path" : dirpath,
-            "watch" : isWatch,
-            "create" : isCreate
-        }))
-        fv.newTree()
-    }
-}
 //
+const socketUrl = `${(location.protocol === 'https:') ? 'wss://' : 'ws://'}${location.hostname}${location.port ? `:${location.port}` : ''}/malafat`;
 document.querySelectorAll('[data-malafat]').forEach((el) => {
-    processFileViewElement(el)
+    FileViewer.fromElement(el, socketUrl)
 })
